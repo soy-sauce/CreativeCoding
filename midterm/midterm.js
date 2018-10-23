916545559ce8005948caf70dcdac7f6ba4530cc4
Connie Zhou <omnawmrawr@gmail.com>
2018-10-22 21:04:30 -0400

    Update midterm.js

--- midterm/midterm.js
+++ midterm/midterm.js
@@ -56,7 +56,6 @@ function draw(){
 
 
   for(var i=0; i<drops.length; i++){
-		drops[i].show();
 		drops[i].move();
   }
   //frameRate(30);
@@ -271,16 +270,7 @@ function Drop(){
 //display drops
 	this.x = random(0,width);
 	this.y = random(-10, height);
-	this.d = 2;
 	this.h = random(2, 10);
-	this.col= map(this.h, 2, 10, 100, 255);
-
-	this.show = function(){
-		noStroke();
-		fill(this.col,100);
-		ellipse(this.x, this.y, this.d, this.h);
-	}
-
 //move object
 	this.vel = 0;
 	this.grv = map(this.h,2,10,3,10);
